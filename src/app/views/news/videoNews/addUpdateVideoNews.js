51bceebecfed1a0e8eaad34f030cb8f09bfab5e0
tanviramoliya <ramoliyatanvi@gmail.com>
2021-02-21 14:47:58 +0530

    Updated Design

--- src/app/views/news/videoNews/addUpdateVideoNews.js
+++ src/app/views/news/videoNews/addUpdateVideoNews.js
@@ -1,68 +1,103 @@
 import React, { Component } from "react";
 import { Breadcrumb } from "components/matx/Breadcrumb";
 import { ValidatorForm } from "react-material-ui-form-validator";
-import { Grid, RadioGroup, FormControlLabel, Radio, Checkbox, Icon, Button, Switch, FormHelperText, MenuItem, Select, FormControl, InputLabel } from "@material-ui/core";
+import {
+  Grid,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Icon,
+  Button,
+  Switch,
+  FormHelperText,
+  MenuItem,
+  Select,
+  FormControl,
+  Input,
+  Chip,
+  InputLabel,
+} from "@material-ui/core";
 import TextValidator from "react-material-ui-form-validator/lib/TextValidator";
-import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import RichTextEditor from "components/matx/RichTextEditor";
-import { Publish } from "@material-ui/icons";
+import SimpleReactValidator from "simple-react-validator";
+
+const names = [
+  "Oliver Hansen",
+  "Van Henry",
+  "April Tucker",
+  "Ralph Hubbard",
+  "Omar Alexander",
+  "Carlos Abbott",
+  "Miriam Wagner",
+  "Bradley Wilkerson",
+  "Virginia Andrews",
+  "Kelly Snyder",
+];
 
 class addUpdateVideoNews extends Component {
+  constructor(props) {
+    super(props);
+    this.validator = new SimpleReactValidator({ autoForceUpdate: this });
+  }
   state = {
-    username: "",
-    firstName: "",
-    email: "",
-    date: new Date(),
-    creditCard: "",
-    mobile: "",
-    password: "",
-    confirmPassword: "",
-    gender: "",
-    agreement: ""
+    link: "",
+    title: "",
+    newsType: "",
+    videoData: "",
+    description: "",
+    publishedBy: "",
+    city: "",
+    state: "",
+    country: "",
+    publish: false,
+    critical: false,
+    tags: [],
+    content: `<h1>Matx | Angular material admin</h1><p><a href="http://devegret.com/" target="_blank"><strong>DevEgret</strong></a></p><p><br></p><p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>`,
   };
 
-  componentDidMount() {
-    // custom rule will have name 'isPasswordMatch'
-    ValidatorForm.addValidationRule("isPasswordMatch", value => {
-      if (value !== this.state.password) {
-        return false;
-      }
-      return true;
-    });
-  }
+  componentDidMount() {}
 
-  componentWillUnmount() {
-    // remove rule when it is not needed
-    ValidatorForm.removeValidationRule("isPasswordMatch");
-  }
+  componentWillUnmount() {}
 
-  handleSubmit = event => {
-    console.log("submitted");
-    console.log(event);
+  handleSubmit = async (event) => {
+    if (
+      this.validator.fieldValid("link") &&
+      this.validator.fieldValid("title") 
+      // this.validator.fieldValid("vision") &&
+      // this.validator.fieldValid("objective") &&
+      // this.validator.fieldValid("heading") &&
+      // this.validator.fieldValid("abstraction") 
+    ){
+      console.log(event);
+    }
+    else {
+      this.validator.showMessages();
+    }
   };
 
-  handleChange = event => {
+  handleChange = (event) => {
     event.persist();
     this.setState({ [event.target.name]: event.target.value });
   };
-
-  handleDateChange = date => {
-    console.log(date);
-
-    this.setState({ date });
+  handleContentChange = (contentHtml) => {
+    this.setState({
+      content: contentHtml,
+    });
   };
-
   render() {
     let {
-      username,
-      firstName,
-      creditCard,
-      mobile,
-      password,
-      confirmPassword,
-      gender,
-      date,
-      email
+      link,
+      title,
+      newsType,
+      videoData,
+      description,
+      publishedBy,
+      city,
+      state,
+      country,
+      publish,
+      critical,
+      tags,
     } = this.state;
     return (
       <div className="m-sm-30">
@@ -71,153 +106,233 @@ class addUpdateVideoNews extends Component {
             routeSegments={[
               { name: "News | Video", path: "/news/videoNews" },
               { name: "Add Video News", path: "/" },
-
             ]}
           />
         </div>
         <ValidatorForm
           ref="form"
           onSubmit={this.handleSubmit}
-          onError={errors => null}
+          onError={(errors) => null}
         >
           <Grid container spacing={6}>
             <Grid item lg={6} md={6} sm={12} xs={12}>
-              <TextValidator
+              <TextField
                 className="mb-16 w-100"
                 label="Embedded YouTube Link"
                 onChange={this.handleChange}
                 type="url"
                 name="link"
-                //value={mobile}
-                validators={["required", "url"]}
-                errorMessages={["this field is required", "enter valid YouTube link"]}
+                error={this.validator.message(
+                  "link",
+                  link,
+                  "required|url"
+                )}
+                helperText={this.validator.message(
+                  "link",
+                  link,
+                  "required|url"
+                )}
+                onBlur={() => this.validator.showMessageFor("link")}
               />
-              <TextValidator
+              <TextField
                 className="mb-16 w-100"
                 label="News Title"
                 onChange={this.handleChange}
                 type="text"
                 name="title"
-                // value={username}
-                validators={[
+                error={this.validator.message(
+                  "title",
+                  title,
                   "required"
-                ]}
-                errorMessages={["this field is required"]}
-              />
-              <TextValidator
-                className="mb-16 w-100"
-                label="First Name"
-                onChange={this.handleChange}
-                type="text"
-                name="firstName"
-                value={firstName}
-                validators={["required"]}
-                errorMessages={["this field is required"]}
-              />
-              <TextValidator
-                className="mb-16 w-100"
-                label="Email"
-                onChange={this.handleChange}
-                type="email"
-                name="email"
-                value={email}
-                validators={["required", "isEmail"]}
-                errorMessages={["this field is required", "email is not valid"]}
-              />
-              <TextValidator
-                className="mb-32 w-100"
-                label="Credit Card"
-                onChange={this.handleChange}
-                type="number"
-                name="creditCard"
-                value={creditCard}
-                validators={[
-                  "required",
-                  "minStringLength:16",
-                  "maxStringLength: 16"
-                ]}
-                errorMessages={["this field is required"]}
+                )}
+                helperText={this.validator.message(
+                  "title",
+                  title,
+                  "required"
+                )}
+                onBlur={() => this.validator.showMessageFor("title")}
               />
             </Grid>
 
             <Grid item lg={6} md={6} sm={12} xs={12}>
-
-              <FormControlLabel className="mt-16"
-                control={<Switch onChange={this.handleChange} name="isPublish" />}
-                label="Publish This News"
-
-              />
-              <FormControlLabel className="mt-16"
-                control={<Switch onChange={this.handleChange} name="isCritical" />}
-                label="Notify To Subscriber"
-              />
-              <FormControl >
-                <InputLabel shrink id="newsType">
-                  News Type</InputLabel>
+              <Grid item xl={12}>
+                <FormControlLabel
+                  className="mt-16"
+                  control={
+                    <Switch onChange={this.handleChange} name="isPublish" />
+                  }
+                  label="Publish This News"
+                />
+                <FormControlLabel
+                  className="mt-16"
+                  control={
+                    <Switch onChange={this.handleChange} name="isCritical" />
+                  }
+                  label="Notify To Subscriber"
+                />
+              </Grid>
+              <Grid item xl={12}>
+                <FormControl className="m-4 pr-16" style={{ width: "32%" }}>
+                  <InputLabel shrink id="newsType">
+                    News Type
+                  </InputLabel>
+                  <Select
+                    labelId="newsType"
+                    id="newsType"
+                    onChange={this.handleChange}
+                    displayEmpty
+                    error={this.validator.message(
+                      "newsType",
+                      newsType,
+                      "required"
+                    )}
+                    helperText={this.validator.message(
+                      "newsType",
+                      newsType,
+                      "required"
+                    )}
+                    onBlur={() => this.validator.showMessageFor("newsType")}
+                  >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                  <FormHelperText style={{color : 'red'}}>{this.validator.message(
+                      "newsType",
+                      newsType,
+                      "required"
+                    )}</FormHelperText>
+                </FormControl>
+                <FormControl className="m-4 pr-16" style={{ width: "32%" }}>
+                  <InputLabel shrink id="category">
+                    Category
+                  </InputLabel>
+                  <Select
+                    labelId="category"
+                    id="category"
+                    onChange={this.handleChange}
+                    displayEmpty
+                  >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                  <FormHelperText>Select Category</FormHelperText>
+                </FormControl>
+                <FormControl className="m-4 pr-16" style={{ width: "32%" }}>
+                  <InputLabel shrink id="subCategory">
+                    SubCategory
+                  </InputLabel>
+                  <Select
+                    labelId="subCategory"
+                    id="subCategory"
+                    onChange={this.handleChange}
+                    displayEmpty
+                  >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                  <FormHelperText>Select SubCategory</FormHelperText>
+                </FormControl>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid sm={12} style={{ padding: "20px 0px" }}>
+            <RichTextEditor
+              content={this.state.content}
+              handleContentChange={this.handleContentChange}
+              placeholder="insert text here..."
+            />
+          </Grid>
+          <Grid container spacing={6}>
+          <Grid item lg={6} md={6} sm={12} xs={12}>
+              <FormControl className="m-4 pr-16" style={{ width: "32%" }}>
+                <InputLabel shrink id="country">
+                  Country
+                </InputLabel>
                 <Select
-                  labelId="newsType"
-                  id="newsType"
-                  //value={age}
+                  labelId="country"
+                  id="country"
                   onChange={this.handleChange}
                   displayEmpty
-                //className={classes.selectEmpty}
                 >
-
                   <MenuItem value={10}>Ten</MenuItem>
                   <MenuItem value={20}>Twenty</MenuItem>
                   <MenuItem value={30}>Thirty</MenuItem>
                 </Select>
-                <FormHelperText>Select News Type</FormHelperText>
+                <FormHelperText>Select Country</FormHelperText>
               </FormControl>
-              <TextValidator
-                className="mb-16 w-100"
-                label="Confirm Password"
-                onChange={this.handleChange}
-                name="confirmPassword"
-                type="password"
-                value={confirmPassword}
-                validators={["required", "isPasswordMatch"]}
-                errorMessages={[
-                  "this field is required",
-                  "password didn't match"
-                ]}
-              />
-              <RadioGroup
-                className="mb-16"
-                value={gender}
-                name="gender"
-                onChange={this.handleChange}
-                row
-              >
-                <FormControlLabel
-                  value="Male"
-                  control={<Radio color="secondary" />}
-                  label="Male"
-                  labelPlacement="end"
-                />
-                <FormControlLabel
-                  value="Female"
-                  control={<Radio color="secondary" />}
-                  label="Female"
-                  labelPlacement="end"
-                />
-                <FormControlLabel
-                  value="Others"
-                  control={<Radio color="secondary" />}
-                  label="Others"
-                  labelPlacement="end"
-                />
-              </RadioGroup>
-
+              <FormControl className="m-4 pr-16" style={{ width: "32%" }}>
+                <InputLabel shrink id="state">
+                  State
+                </InputLabel>
+                <Select
+                  labelId="state"
+                  id="state"
+                  onChange={this.handleChange}
+                  displayEmpty
+                >
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+                <FormHelperText>Select State</FormHelperText>
+              </FormControl>
+              <FormControl className="m-4 pr-16" style={{ width: "32%" }}>
+                <InputLabel shrink id="city">
+                  City
+                </InputLabel>
+                <Select
+                  labelId="city"
+                  id="city"
+                  onChange={this.handleChange}
+                  displayEmpty
+                >
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+                <FormHelperText>Select City</FormHelperText>
+              </FormControl>
             </Grid>
+            <Grid item lg={6} md={6} sm={12} xs={12}>
+              <FormControl>
+                <InputLabel id="tags">Tags</InputLabel>
+                <Select
+                  labelId="tags"
+                  id="tags"
+                  name="tags"
+                  multiple
+                  value={tags}
+                  onChange={this.handleChange}
+                  input={<Input id="select-multiple-chip" />}
+                  renderValue={(selected) => (
+                    <div>
+                      {selected.map((value) => (
+                        <Chip key={value} label={value} />
+                      ))}
+                    </div>
+                  )}
+                  // MenuProps={MenuProps}
+                >
+                  {names.map((name) => (
+                    <MenuItem key={name} value={name}>
+                      {name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
           </Grid>
-          <Button color="primary" variant="contained" type="submit">
-            <Icon>send</Icon>
-            <span className="pl-8 capitalize">Send</span>
+          </Grid>
+
+          <Button color="primary" variant="contained" type="submit" className="mt-12">
+            <Icon>add</Icon>
+            <span className="pl-8 capitalize">Add News</span>
           </Button>
         </ValidatorForm>
       </div>
-    )
+    );
   }
 }
 export default addUpdateVideoNews;
